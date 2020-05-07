75e164eda377bfceb18473aff36009dbca4c8453
Aksha <aksha.ali@daffodilsw.com>
2020-05-07 12:27:58 +0530

    add to existing repo

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-//import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from './serviceWorker';
 import {BrowserRouter} from 'react-router-dom';
 import { createStore } from 'redux';
 import userData from './redux/reducer';
@@ -12,37 +12,7 @@ import {Provider} from 'react-redux';
 const globalStore = createStore(userData);
 console.log("globalStore", globalStore);
 
-//ACTION
-// const increament = (data) => {
-//     return{
-//         type:'INCREAMENT',
-//         data:data
-//     }
-//  }
-// const decreament = () => {
-//     return {
-//         type:'DECREAMENT'
-//     }
-// }
 
-// //REDUCER
-// const counter = (state = [], action) => {
-//     switch(action.type){
-//         case "INCREAMENT" :
-//             return state = action.data;
-//         case 'DECREAMENT' :
-//             return state - 1;
-//     }
-// };
-
-// let store = createStore(counter);
-// store.subscribe(()=>{
-//     console.log("redux just worked", store.getState());
-// })
-
-// //DISPATCH
-// store.dispatch(increament([1,2,3,8,9]));
-// store.dispatch(increament());
 
 ReactDOM.render(<BrowserRouter>
 <Provider store={globalStore}>
@@ -53,4 +23,4 @@ ReactDOM.render(<BrowserRouter>
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-//serviceWorker.unregister();
+serviceWorker.unregister();
